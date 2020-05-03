01e175caefbbb0b1f0e7d77a9830db6e4e625119
mate00 <mami.ext@gmail.com>
2020-05-03 07:33:22 +0200

    Faulty contorller and simple controller level exception handler.

---
+++ src/main/java/rnd/mate00/exceptionhandler/FaultyController.java
@@ -0,0 +1,30 @@
+package rnd.mate00.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FaultyController {
+
+    @GetMapping("/element/{id}")
+    public ResponseEntity<String> getElement(@PathVariable("id") int id) {
+        if (id > 100) {
+            throw new IllegalArgumentException("Input value too big");
+        }
+
+        return new ResponseEntity<>("Proper element", HttpStatus.ACCEPTED);
+    }
+
+    /*
+    Active only for this controller.
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public void handle() {
+        System.out.println("Handling exception");
+    }
+
+}
---
+++ src/test/java/rnd/mate00/exceptionhandler/SpringExceptionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package rnd.mate00.exceptionhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExceptionHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
