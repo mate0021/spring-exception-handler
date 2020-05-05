4ed27ebf61bdef521a12909d74f60af878517dbd
mate00 <mami.ext@gmail.com>
2020-05-05 06:32:58 +0200

    Different type of exceptions handled in separate methods.

--- src/main/java/rnd/mate00/exceptionhandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/rnd/mate00/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -11,10 +11,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler { //extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ IllegalArgumentException.class, EntityNotFoundException.class} )
+    @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<String> handleExceptionInternal(Exception ex, WebRequest request) {
-        System.out.println("Handling in controller advice");
+        System.out.println("Handling illegal argument in controller advice");
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There was an issue");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There was an issue with argument");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<String> handleEntityNotFound(Exception ex, WebRequest request) {
+        System.out.println("Handling entity not found in controller advice");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("We didn't find your entity");
     }
 }
